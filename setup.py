5c38618eabe86f866547283c600c0ed2d0f63d65
Peter Edwards <peadar@arista.com>
2019-11-28 15:38:41 +0000

    Convert binary to string in a python{2,3} friendly way

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import subprocess
 
 pipe = subprocess.Popen( [ "uname", "-i" ], stdout=subprocess.PIPE )
 ( out, err ) = pipe.communicate()
-arch = out.strip()
+arch = str(out.decode("utf-8")).strip()
 
 text = ""
 
